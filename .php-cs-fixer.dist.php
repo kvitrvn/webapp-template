c03b1518c84d342c37166382f1943c3236d8838e
Benjamin Gaudé <bgaude.dev@protonmail.com>
2022-05-28 08:46:56 +0200

    Ajout de php-cs-fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,17 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/src')
+    ->in(__DIR__.'/tests')
+    ->exclude('var')
+;
+
+$config = new PhpCsFixer\Config();
+
+return $config
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+    ])
+    ->setFinder($finder)
+;
